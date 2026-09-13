4660d93e4b1a0d143f18a34c28754ee1cc50d274
Shafqath <90248184+Shafqath00@users.noreply.github.com>
2026-09-13 22:11:53 +0530

    feat: export custom data and dynamic source contracts

--- jellyshops/packages/storefront-schema/src/index.ts
+++ jellyshops/packages/storefront-schema/src/index.ts
@@ -11,5 +11,28 @@ export {
   storefrontDocumentSchema,
   validateStorefrontDocument,
 } from "./storefront-v2.js";
+export {
+  customDataOwnerTypeSchema,
+  dynamicScalarValueTypes,
+  dynamicValueTypeSchema,
+  metafieldDefinitionSchema,
+  metaobjectDefinitionSchema,
+  metaobjectFieldDefinitionSchema,
+} from "./custom-data.js";
+export {
+  dynamicBindingSchema,
+  dynamicResourceTypeSchema,
+  isDynamicValueTypeCompatible,
+  settingValueSchema,
+} from "./dynamic-sources.js";
 export type { MediaReference, StorefrontDocument, StorefrontPage, StorefrontPageType, StorefrontTemplateId } from "./storefront-v2.js";
+export type {
+  CustomDataOwnerType,
+  DynamicScalarValueType,
+  DynamicValueType,
+  MetafieldDefinitionContract,
+  MetaobjectDefinitionContract,
+  MetaobjectFieldDefinitionContract,
+} from "./custom-data.js";
+export type { DynamicBinding, DynamicResourceType, SettingValue } from "./dynamic-sources.js";
 export type { BlockNode, GlobalSettings, PageDocument, PageType, SectionNode, StoreDesignDocument, ThemeId } from "./types.js";
