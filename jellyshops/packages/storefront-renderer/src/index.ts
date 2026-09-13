2b20bc980b80a902bb33f3d6ba42d45c3e450200
Shafqath <90248184+Shafqath00@users.noreply.github.com>
2026-09-14 00:46:25 +0530

    feat: export registry-driven renderer API

--- jellyshops/packages/storefront-renderer/src/index.ts
+++ jellyshops/packages/storefront-renderer/src/index.ts
@@ -1,3 +1,6 @@
 export { StorefrontRenderer } from "./storefront-renderer";
+export { RegistrySectionRenderer } from "./registry-section-renderer";
+export { SectionRenderRegistry, defaultSectionRenderRegistry } from "./render-registry";
 export { isRenderableHomeSection, renderableHomeSectionTypes } from "./section-support";
+export type { SectionRenderComponent, SectionRenderProps } from "./render-registry";
 export type { AssetResolver, CommerceDataProvider, PublicProduct, StorefrontRendererProps } from "./types";
